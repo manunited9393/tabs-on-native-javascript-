f191c8831225b54765ac04883b89d84ca16bd344
manunited9393 <56319570+manunited9393@users.noreply.github.com>
2020-02-18 20:45:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 window.addEventListener("DOMContentLoaded", function() {
     'use strict';
     
-    let content = document.querySelectorAll('.header__right-side'),  //your tabcontent class
-        navTab = document.querySelector('.header__brend'),           //your tabs' parent (e.g. "ul")
-        tab = document.querySelectorAll('.header__brend-item');      //your tabs
+    let content = document.querySelectorAll('.content__for_tabs'),  //your tabcontent class
+        navTab = document.querySelector('.tabs__parent'),           //your tabs' parent (e.g. "ul")
+        tab = document.querySelectorAll('.tab');                    //your tabs (e.g. li)
 
     function removeContent(a) {
         for ( let i = a; i < content.length; i++) {
-            content[i].classList.remove('show');
-            content[i].classList.add("hide");
+            content[i].classList.remove('show');       //show is css class "display block"
+            content[i].classList.add("hide");          //hide is css class "display none"
         }
     }
     removeContent(1);
 
     function showContent(b) {
-        if (content[b].classList.contains('hide')) {
-            content[b].classList.remove('hide');
-            content[b].classList.add("show");
+        if (content[b].classList.contains('hide')) {   //hide is css class "display none"
+            content[b].classList.remove('hide');       //hide is css class "display none"
+            content[b].classList.add("show");          //show is css class "display block"
         }
     }
 
